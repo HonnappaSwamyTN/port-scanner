47e6a9bb0b8c17f798fe0dd941918010f1383296
HonnappaSwamyTN <honnappaswamytn@gmail.com>
2026-04-24 21:54:29 +0530

    Added Banner grabbing — extracts service and version information   from open ports

--- README.md
+++ README.md
@@ -8,9 +8,16 @@ Scans port ranges and identifies open ports on a target system.
 - Configurable timeout parameter
 - Scans ports from 1 to 1024
 - Identifies open and closed ports
+- Banner grabbing — extracts service and version information 
+  from open ports
+- - Stores and displays all open ports at end of scan
 
 ## How to Run
 python port_scanner.py
+
+When prompted:
+- Enter target IP address or domain (e.g. 127.0.0.1 or scanme.nmap.org)
+- Optional: configure timeout when creating scanner object
 
 ## Concepts Used
 - Socket programming
--- port_scanner.py
+++ port_scanner.py
@@ -5,32 +5,50 @@ class PortScanner:
         # store the target IP here
         self.target_ip=target
         self.time_out=time_out
+        self.open_ports=[]
 
     def scan_port(self, port):
         
         # attempt connection to target:port
+        # return True if open, False if closed
         with socket.socket() as s:
             s.settimeout(self.time_out)
             result=s.connect_ex((self.target_ip,port))
             
 
             return result==0
-        # return True if open, False if closed
 
     def scan_range(self, start_port, end_port):
+        # loop through ports start to end
+        # call scan_port for each
+        # print whether open or closed
 
         for i in range(start_port,end_port+1):
             val=self.scan_port(i)
 
             if val:
                 print(f"Port {i} is Open")
+                self.open_ports.append(i)
+                banner=self.grab_banner(i)
+                if banner:
+                    print(f"Banner {banner}")
             else:
                 print(f"Port {i} is closed")
+        print("Open ports are : ")
+        print(self.open_ports)
 
-        # loop through ports start to end
-        # call scan_port for each
-        # print whether open or closed
+    def grab_banner(self, port):
+        try:
+            with socket.socket() as s:
+                s.settimeout(self.time_out)
+                if s.connect_ex((self.target_ip,port)) == 0:
+                    data = s.recv(1024)
+                    return data.decode()
+        except(socket.error, UnicodeDecodeError):
+            return None
+    
+
 
 target = input("Enter target IP: ")
-scanner = PortScanner(target)
+scanner = PortScanner("google.com", time_out=1)
 scanner.scan_range(1, 1024)

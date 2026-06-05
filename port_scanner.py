2947c9a951a8bd60549d91b84e89bfb26e300771
HonnappaSwamyTN <honnappaswamytn.com>
2026-06-05 15:34:43 +0530

    v2.0 - added threading, lock, service identification

--- port_scanner.py
+++ port_scanner.py
@@ -1,4 +1,6 @@
 import socket
+from concurrent.futures import ThreadPoolExecutor
+import threading
 
 class PortScanner:
     def __init__(self, target,time_out=0.05,banner_timeout=2):
@@ -7,6 +9,7 @@ class PortScanner:
         self.time_out=time_out
         self.banner_timeout=banner_timeout
         self.open_ports=[]
+        self.lock=threading.Lock()
 
     def scan_port(self, port):
         
@@ -18,22 +21,48 @@ class PortScanner:
             
 
             return result==0
+    
+    def single_port_processor(self, port):
+        val=self.scan_port(port)
+        if val:
+            try:
+                serv=socket.getservbyport(port)
+            except (OSError,OverflowError):
+                serv="Unknown service"
+            
+            banner=self.grab_banner(port)
+            if banner:
+                print(f"Banner {banner}")
+            
+            with self.lock:
+                self.open_ports.append(port)
+            
+            print(f"port : {port},Service : {serv}")
+
 
     def scan_range(self, start_port, end_port):
-        # loop through ports start to end
-        # call scan_port for each
-        # print whether open or closed
+        #loop through ports start to end
+        #call scan_port for each
+        #print whether open or closed
 
-        for i in range(start_port,end_port+1):
-            val=self.scan_port(i)
+        # for i in range(start_port,end_port+1):  #this is sequential scanning time consuming
+        #     val=self.scan_port(i)
 
-            if val:
+        #     if val:
+        #         try:
+        #             serv = socket.getservbyport(i)
+        #         except (OSError, OverflowError):
+        #             serv="Unknown"
                
-                print(f"Port {i} is Open")
-                self.open_ports.append(i)
-                banner=self.grab_banner(i)
-                if banner:
-                    print(f"Banner {banner}")
+        #         print(f"Port {i} is Open and provides the service : {serv}")
+        #         self.open_ports.append(i)
+        #         banner=self.grab_banner(i)
+        #         if banner:
+        #             print(f"Banner {banner}")
+
+        #This is threadding it is faster than sequential scanning
+        with ThreadPoolExecutor(max_workers=100) as executor:
+            executor.map(self.single_port_processor,range(start_port,end_port+1))
 
     def grab_banner(self, port):
         try:

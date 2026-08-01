9c56d355152fb7f65a7ba9f7d98c5a6314f05e85
HonnappaSwamyTN <honnappaswamytn.com>
2026-08-01 16:20:41 +0530

    fix: grab_banner sends HTTP probe, fixes the double connection and also added banner timeout.

--- port_scanner.py
+++ port_scanner.py
@@ -14,69 +14,57 @@ class PortScanner:
     def scan_port(self, port):
         
         # attempt connection to target:port
-        # return True if open, False if closed
-        with socket.socket() as s:
-            s.settimeout(self.time_out)
-            result=s.connect_ex((self.target_ip,port))
-            
+        # return True if open, False if close
+        s=socket.socket()
+        s.settimeout(self.time_out)
+        result=s.connect_ex((self.target_ip,port))
 
-            return result==0
+        if result == 0:
+            return s
+        else:
+            s.close()
+            return None
     
     def single_port_processor(self, port):
         val=self.scan_port(port)
         if val:
             try:
-                serv=socket.getservbyport(port)
-            except (OSError,OverflowError):
-                serv="Unknown service"
+                try:
+                    serv=socket.getservbyport(port)
+                except (OSError,OverflowError):
+                    serv="Unknown service"
+                
+                banner=self.grab_banner(port,val)
+                if banner:
+                    print(f"Banner {banner}")
+                
+                with self.lock:
+                    self.open_ports.append(port)
+                    
+                print(f"port : {port},Service : {serv}")
             
-            banner=self.grab_banner(port)
-            if banner:
-                print(f"Banner {banner}")
-            
-            with self.lock:
-                self.open_ports.append(port)
-            
-            print(f"port : {port},Service : {serv}")
+            finally:
+                val.close()
 
 
     def scan_range(self, start_port, end_port):
-        #loop through ports start to end
-        #call scan_port for each
-        #print whether open or closed
-
-        # for i in range(start_port,end_port+1):  #this is sequential scanning time consuming
-        #     val=self.scan_port(i)
-
-        #     if val:
-        #         try:
-        #             serv = socket.getservbyport(i)
-        #         except (OSError, OverflowError):
-        #             serv="Unknown"
-               
-        #         print(f"Port {i} is Open and provides the service : {serv}")
-        #         self.open_ports.append(i)
-        #         banner=self.grab_banner(i)
-        #         if banner:
-        #             print(f"Banner {banner}")
-
-        #This is threadding it is faster than sequential scanning
         with ThreadPoolExecutor(max_workers=100) as executor:
             executor.map(self.single_port_processor,range(start_port,end_port+1))
 
-    def grab_banner(self, port):
+    def grab_banner(self, port, sock):
         try:
-            with socket.socket() as s:
-                s.settimeout(self.banner_timeout)
-                if s.connect_ex((self.target_ip,port)) == 0:
-                    data = s.recv(1024)
-                    return data.decode()
+            sock.settimeout(self.banner_timeout)
+            http_ports = [80, 443, 8080, 8443]
+            if port in http_ports:
+                sock.send(("GET / HTTP/1.1\r\nHost: " + self.target_ip + "\r\n\r\n").encode("utf-8"))
+            data = sock.recv(1024)
+            return data.decode()
         except(socket.error, UnicodeDecodeError):
             return None
     
 
 
 target = input("Enter target IP: ")
-scanner = PortScanner(target)
+scanner = PortScanner(target, time_out=1)
 scanner.scan_range(1, 1024)
 print("----------END----------")
